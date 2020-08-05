23fb9a09611928fe37cfb3115e33ce939fb4cc0f
Alexander <kharchenko7717@gmail.com>
2020-08-05 13:41:02 +0300

    prod

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,43 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container-fluid pt-5">
+    <input v-model="searchLine" @keyup.enter="searchCall" ref="searchLine" placeholder="Search images" class="form-control">
+    <div v-if="Array.isArray(hits) && hits.length" class="card-columns mt-3">
+      <Post v-for="i of hits" :key="i.pageURL" :data="i" />
+    </div>
+    <div v-else-if="Array.isArray(hits) && !hits.length" class="text-center">
+      <h3 class="text-muted font-weight-light mt-5">Nothing was found</h3>
+    </div>
+    <div v-else class="text-center">
+      <h3 class="text-muted font-weight-light mt-5">Write anything and press enter...</h3>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios'
+import Post from '@/components/Post'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Post
+  },
+  data: () => ({
+    searchLine: '',
+    hits: null
+  }),
+  mounted () {
+    this.$refs.searchLine.focus() // focus on input when mounted
+  },
+  methods: {
+    async searchCall () {
+      const filteredSearchLine = this.searchLine.replace(/ /g, '+')
+      const response = await axios.get(
+        `https://pixabay.com/api/?key=17779624-c7b9998197af18a5bd96c56cc&q=${filteredSearchLine}`
+      )
+
+      this.hits = response.data.hits
+    }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/Post.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="card px-0">
+    <a :href="data.pageURL">
+      <img class="card-img-top" :src="data.webformatURL" alt="Image">
+    </a>
+    <div class="card-body row py-2 text-muted">
+      <div class="col-lg-6">
+        <small>🏷️ {{ data.tags }}</small>
+      </div>
+      <div class="col-lg-6 text-lg-right">
+        <small>id: {{ data.id }}</small>
+      </div>
+    </div>
+    <div class="card-footer row mx-0 px-0 py-2">
+      <div class="col-lg-6">
+        <small>&#128100; {{ data.user }}</small>
+      </div>
+      <div class="col-lg-6 text-lg-right">
+        <small>❤️ {{ data.likes }}</small>
+        <small class="mx-1"> ⭐ {{ data.favorites }}</small>
+        <small> 💭 {{ data.comments }}</small>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Post',
+  props: {
+    data: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
